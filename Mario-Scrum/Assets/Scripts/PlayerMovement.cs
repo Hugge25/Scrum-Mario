8aa3bec5c2eee45de1d6a45de224078b836b232c
Hugge25 <hugo.paulsson@hotmail.com>
2023-10-11 10:52:42 +0200

    ez

--- Mario-Scrum/Assets/Scripts/PlayerMovement.cs
+++ Mario-Scrum/Assets/Scripts/PlayerMovement.cs
@@ -8,34 +8,45 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private float jump = 500;
     private float speed = 5f;
+
+    private bool canjump;
     void Start()  // Start is called before the first frame update
     {
         rb = GetComponent<Rigidbody2D>();
+        canjump = true;
     }
 
-    void Update() // Update is called once per frame
+    void FixedUpdate() // Update is called once per frame
     {
 
         Vector3 movement = new Vector3();
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && canjump == true)
         {
             rb.AddForce(new Vector2(rb.velocity.x, jump));
+            canjump = false;
+            StartCoroutine(JumpReset());
         }
         if(Input.GetKey(KeyCode.S))
         {
             
         }
-        if(Input.GetKey(KeyCode.A))
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             movement.x = -1;
         }
-        if(Input.GetKey(KeyCode.D))
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             movement.x = 1;
         }
-
+    
         movement.Normalize();
         transform.position += movement * Time.deltaTime * speed;
+    }
+
+    IEnumerator JumpReset()
+    {
+        yield return new WaitForSeconds(1.5f);
+        canjump = true;
     }
 }
